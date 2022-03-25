f31d2c983cab85ed37837946487ce711f9fc3ae2
maxhyeon <americano314@gmail.com>
2022-03-25 14:15:52 +0900

    without deploy

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'hello world-without deploy',
                 // location: ret.data.trim()
             })
         }
