4944aca872cfd7cd84a2a9c2047fc9a4e43f7c7f
maxhyeon <americano314@gmail.com>
2022-03-25 13:26:08 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+let response;
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const app = express();
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
